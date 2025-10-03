173d3b37b1075d1cbb459c97b3fa8361bef2e351
kanshuYokoo <k-yokoo@seiwa-stss.jp>
2025-10-03 14:46:34 +0900

    add type to variable and function return

--- config/database_config.py
+++ config/database_config.py
@@ -1,6 +1,14 @@
 class DatabaseConfig:
-    def __init__(self):
-        self.host = ""
-        self.user = ""
-        self.password = ""
-        self.database = ""
+    def __init__(self, host: str = "", user: str = "", password: str = "", database: str = "") -> None:
+        self.host: str = host
+        self.user: str = user
+        self.password: str = password
+        self.database: str = database
+    
+    def to_dict(self) -> dict[str, str]:
+        return {
+            'host': self.host,
+            'user': self.user,
+            'password': self.password,
+            'database': self.database
+        }
--- src/query/query_executor.py
+++ src/query/query_executor.py
@@ -1,10 +1,13 @@
-class QueryExecutor:
-    def __init__(self, connection):
-        self.connection = connection
+from typing import List, Tuple, Any
+from mysql.connector import MySQLConnection
 
-    def execute_query(self, query):
+class QueryExecutor:
+    def __init__(self, connection: MySQLConnection) -> None:
+        self.connection: MySQLConnection = connection
+
+    def execute_query(self, query: str) -> List[Tuple[Any, ...]]:
         cursor = self.connection.cursor()
         cursor.execute(query)
-        results = cursor.fetchall()
+        results: List[Tuple[Any, ...]] = cursor.fetchall()
         cursor.close()
         return results
